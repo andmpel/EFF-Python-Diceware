410d6e3c48d08c70d92a5dc5b254d7630fa52b7a
Drew <9892074+andmpel@users.noreply.github.com>
2024-11-27 14:01:06 -0600

    Update dice.py

--- dice.py
+++ dice.py
@@ -9,21 +9,12 @@ def rollx5():
     return ''.join(str(roll()) for _ in range(5))
 
 def wordDict():
-    try:
         with open('eff_large_wordlist.txt') as f:
             return {key: val for key, val in (line.strip().split() for line in f)}
-    except FileNotFoundError:
-        print("Word list file 'eff_large_wordlist.txt' not found.")
-        exit(1)
 
 def genWords(wordCount = int(input("Enter number of words to generate: "))):
-    d = wordDict()
-    words = []
-    for _ in range(wordCount):
-        wordKey = rollx5()
-        word = d.get(wordKey)
-        words.append(word)
-    print(' '.join(words))
+    words = ' '.join([wordDict().get(str(rollx5())) for _ in range(wordCount)])
+    print(words)
 
 if __name__ == '__main__':
     genWords()
